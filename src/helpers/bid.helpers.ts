b7b6d4a02929b8ec900b24d89d6e8a6e632b62e6
Alan Płócieniak <alan-null@outlook.com>
2017-11-06 23:06:16 +0100

    Implemented bid validator (canBid)

--- src/helpers/bid.helpers.ts
+++ src/helpers/bid.helpers.ts
@@ -1,15 +1,23 @@
 import { PlayersBid, Game, Player } from '../game.interfaces';
 import * as _ from 'lodash';
 
-export function isMaxBid({bid}: PlayersBid): boolean {
+export function isMaxBid({ bid }: PlayersBid): boolean {
     return bid === 300;
+}
+
+export function isAchievableBid({ bid }: PlayersBid): boolean {
+    return bid <= 300;
+}
+
+export function isValidBidValue({ bid }: PlayersBid): boolean {
+    return parseInt(bid + "") === parseFloat(bid + "") && bid % 10 == 0;
 }
 
 export function hasTwoPasses(bids: PlayersBid[]): boolean {
     return _.chain(bids)
-    .filter((playerBid: PlayersBid) => playerBid.pass)
-    .thru(passedBids => passedBids.length === 2)
-    .value();
+        .filter((playerBid: PlayersBid) => playerBid.pass)
+        .thru(passedBids => passedBids.length === 2)
+        .value();
 }
 
 export function getBidWinner(bids: PlayersBid[]): PlayersBid {
@@ -20,4 +28,8 @@ export function getUniqueBidders(bids: PlayersBid[]): string[] {
     return _.reduce(bids, (bidders, bid) => {
         return bidders.includes(bid.player) ? bidders : [bid.player, ...bidders];
     }, []);
+}
+
+export function getHighestBid(bids: PlayersBid[]): PlayersBid {
+    return _.max(bids, function (pb) { return pb.bid; });
 }
