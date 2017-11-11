b0badb39d19b78720efc88756042895ddd73a2f2
Alan Płócieniak <alan-null@outlook.com>
2017-11-11 13:43:07 +0100

    #4 | Unit tests for game actions

--- test/actions/game.actions.ts
+++ test/actions/game.actions.ts
@@ -2,7 +2,7 @@ import * as should from 'should';
 import { Game, Phase } from '../../src/game.interfaces';
 import { createCard } from '../../src/helpers/cards.helpers';
 import { isBiddingFinished, canBid } from '../../src/validators/bid.validator';
-import { bid, setPhase, dealCardToStock, dealCardToPlayer } from '../../src/game.actions';
+import { bid, setPhase, dealCardToStock, dealCardToPlayer, registerPlayer, setDeck, shareStock, assignStock } from '../../src/game.actions';
 import { game as gameReducer } from '../../src/game.reducer';
 
 describe('actions', () => {
@@ -35,7 +35,7 @@ describe('actions', () => {
             const currentState = this.state;
             const nextState = gameReducer(currentState, setPhase(Phase.BIDDING_START));
             currentState.phase = Phase.BIDDING_START;
-    
+
             should(nextState).be.deepEqual(this.state);
         });
     });
@@ -51,7 +51,7 @@ describe('actions', () => {
             ];
             currentState.stock = [createCard('J♥')];
             const nextState = gameReducer(currentState, dealCardToStock());
-    
+
             should(nextState.stock[0]).be.deepEqual(createCard('A♥'), createCard('J♥'));
             should(nextState.deck).be.deepEqual([createCard('9♥'), createCard('J♦')]);
         });
@@ -68,21 +68,21 @@ describe('actions', () => {
             currentState.cards['adam'] = [createCard('J♥')];
 
             const nextState = gameReducer(currentState, dealCardToPlayer('adam'));
-    
+
             should(nextState.cards['adam']).be.deepEqual([createCard('A♥'), createCard('J♥')]);
             should(nextState.deck).be.deepEqual([createCard('9♥'), createCard('J♦')]);
         });
-    });    
+    });
 
-    
+
 
     describe('bid', () => {
         it('add bid', () => {
             const currentState = this.state;
             const nextState = gameReducer(currentState, bid('alan', 120));
-    
+
             should(nextState.bid[0]).be.deepEqual({
-                player: 'alan', 
+                player: 'alan',
                 bid: 120,
                 pass: false
             });
@@ -90,36 +90,75 @@ describe('actions', () => {
         it('add pass bid', () => {
             const currentState = this.state;
             const nextState = gameReducer(currentState, bid('alan', 0));
-    
+
             should(nextState.bid[0]).be.deepEqual({
-                player: 'alan', 
+                player: 'alan',
                 bid: 0,
                 pass: true
             });
         });
     });
 
-    xdescribe('registerPlayer', () => {
-        it('', () => {
-            // @TODO write test to registerPlayer action
+    describe('registerPlayer', () => {
+        it('add new player to board', () => {
+            // assign
+            const currentState = this.state;
+            const playerId = "dżesika";
+            // act
+            const nextState = gameReducer(currentState, registerPlayer(playerId));
+            //assert
+            should(nextState.players.length).be.equal(4);
+            should(nextState.players[3].id).be.equal(playerId);
+
         });
     });
 
-    xdescribe('setDeck', () => {
-        it('', () => {
-            // @TODO write test to setDeck action
+    describe('setDeck', () => {
+        it('set game deck', () => {
+            // assign
+            const currentState = this.state;
+            const deck = currentState.deck
+            deck.push(createCard('J♥'))
+            // act
+            const nextState = gameReducer(currentState, setDeck(deck));
+            //assert
+            should(nextState.deck).be.deepEqual(deck);
+            should(nextState.deck[3]).be.deepEqual({ rank: 'J', suit: '♥' });
         });
-    }); 
+    });
 
-    xdescribe('shareStock', () => {
-        it('', () => {
-            // @TODO write test to shareStock action
+    describe('shareStock', () => {
+        it('share card', () => {
+            // assign
+            const currentState = this.state;
+            currentState.cards['pic'] = [createCard('10♥'), createCard('10♦')];
+            currentState.cards['adam'] = [];
+            // act
+            const nextState = gameReducer(currentState, shareStock(currentState.cards.pic[0], 'adam'));
+            //assert
+            should(nextState.cards['pic'].length).be.equal(1);
+            should(nextState.cards['pic'][0]).be.deepEqual({ rank: '10', suit: '♦' });
+            should(nextState.cards['adam'].length).be.equal(1);
+            should(nextState.cards['adam'][0]).be.deepEqual({ rank: '10', suit: '♥' });
+
         });
-    });    
-    
-    xdescribe('assignStock', () => {
-        it('', () => {
-            // @TODO write test to assignStock action
+    });
+
+    describe('assignStock', () => {
+        it('assign stock to a bid winner', () => {
+            // assign
+            const currentState = this.state;
+            currentState.cards.pic = [];
+            currentState.stock = [
+                createCard('10♥'),
+                createCard('10♥'),
+                createCard('J♦')
+            ];
+            // act
+            const nextState = gameReducer(currentState, assignStock());
+            //assert
+            should(nextState.cards['pic'].length).be.equal(3);
+            should(nextState.cards['pic']).be.deepEqual(currentState.stock);
         });
     });
 });
