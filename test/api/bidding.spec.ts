e0cf88b9b87a2f4d93bca7e8328eabf48ac6d907
adasq <asnaebaem@gmail.com>
2017-12-10 21:17:55 +0100

    .

--- test/api/bidding.spec.ts
+++ test/api/bidding.spec.ts
@@ -4,7 +4,7 @@ import * as should from 'should';
 import { createCards } from '../../src/helpers/cards.helpers';
 import { SHARE_STOCK } from '../../src/game.actions';
 
-describe('bidding', () => {
+describe.only('bidding', () => {
     it('sets cards and initializes bidding process', () => {
 
         const history = [];
@@ -34,13 +34,20 @@ describe('bidding', () => {
         }
 
         const thousand: Thousand = initializeGame(initState);
-        
+
         thousand.events.addListener('phaseUpdated', next => {
             const state: Game = thousand.getState();
 
             history.push([state.phase, state.bid]);
 
-            if(state.phase === Phase.BIDDING_FINISHED) {
+            const expectedFinallBiddingState = [
+                { player: 'pic', bid: 0, pass: true } as PlayersBid,
+                { player: 'alan', bid: 120, pass: false } as PlayersBid,
+                { player: 'adam', bid: 0, pass: true } as PlayersBid,
+                { player: 'pic', bid: 100, pass: false } as PlayersBid
+            ];
+
+            if (state.phase === Phase.SHARE_STOCK) {
                 should(history).be.deepEqual([
                     [Phase.BIDDING_START, []],
                     // this is set by default:
@@ -48,7 +55,7 @@ describe('bidding', () => {
                         { player: 'pic', bid: 100, pass: false } as PlayersBid,
                     ]],
                     // thousand.pass('adam'):
-                    [Phase.BIDDING_IN_PROGRESS, [                        
+                    [Phase.BIDDING_IN_PROGRESS, [
                         { player: 'adam', bid: 0, pass: true } as PlayersBid,
                         { player: 'pic', bid: 100, pass: false } as PlayersBid,
                     ]],
@@ -61,18 +68,11 @@ describe('bidding', () => {
                     // thousand.bid('adam', 130):
                     // no event propagated, as it's not allowed
                     // thousand.pass('pic'):
-                    [Phase.BIDDING_IN_PROGRESS, [
-                        { player: 'pic', bid: 0, pass: true } as PlayersBid,
-                        { player: 'alan', bid: 120, pass: false } as PlayersBid,
-                        { player: 'adam', bid: 0, pass: true } as PlayersBid,
-                        { player: 'pic', bid: 100, pass: false } as PlayersBid
-                    ]],
-                    [Phase.BIDDING_FINISHED, [
-                        { player: 'pic', bid: 0, pass: true } as PlayersBid,
-                        { player: 'alan', bid: 120, pass: false } as PlayersBid,
-                        { player: 'adam', bid: 0, pass: true } as PlayersBid,
-                        { player: 'pic', bid: 100, pass: false } as PlayersBid
-                    ]]
+                    [Phase.BIDDING_IN_PROGRESS, expectedFinallBiddingState],
+                    [Phase.BIDDING_FINISHED, expectedFinallBiddingState],
+                    [Phase.FLIP_STOCK, expectedFinallBiddingState],
+                    [Phase.ASSIGN_STOCK, expectedFinallBiddingState],
+                    [Phase.SHARE_STOCK, expectedFinallBiddingState]
                 ]);
             }
             next();
@@ -86,15 +86,68 @@ describe('bidding', () => {
             thousand.bid('adam', 130),
             thousand.pass('pic')
         ];
-        
+
         should(actionsResult).be.deepEqual([
             true,
             true,
             false, // adam can't bid 130, as it's not his turn!
             true
         ]);
+    });
 
-        // the next step after all is sharing stock phase
-        should(thousand.getState().phase).be.equal(Phase.SHARE_STOCK);
+    it('skips FLIP_STOCK phase, when all bidders rejected to bid higher than 100', () => {
+
+        const history = [];
+        const initState: Game = {
+            settings: {
+                permitBombOnBarrel: true,
+                maxBombs: 2,
+                barrelPointsLimit: 880
+            },
+            phase: Phase.BIDDING_FINISHED,
+            players: [
+                { id: 'adam', battlePoints: [120, null] },
+                { id: 'alan', battlePoints: [0, 60] },
+                { id: 'pic', battlePoints: [0, 60] }
+            ],
+            deck: [],
+            stock: [
+                ...createCards(3)
+            ],
+            bid: [
+                { player: 'alan', bid: 0, pass: true },
+                { player: 'adam', bid: 0, pass: true },
+                { player: 'pic', bid: 100, pass: false }
+            ] as PlayersBid[],
+            cards: {
+                'adam': createCards(7),
+                'alan': createCards(7),
+                'pic': createCards(7)
+            },
+            battle: null
+        }
+
+
+        const thousand: Thousand = initializeGame(initState);
+        
+        thousand.events.addListener('phaseUpdated', next => {
+            const state: Game = thousand.getState();
+           
+            
+            history.push(state.phase);
+
+            if (state.phase === Phase.SHARE_STOCK) {
+                should(history).be.deepEqual([
+                    Phase.BIDDING_FINISHED,
+                    Phase.ASSIGN_STOCK,
+                    Phase.SHARE_STOCK
+                ]);
+            }
+
+            next();
+
+        });
+        
+        thousand.init();
     });
 });
