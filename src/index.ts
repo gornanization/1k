f458828112f512fa246881753637c9570e032edf
adasq <asnaebaem@gmail.com>
2017-12-18 19:25:26 +0100

    .

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { getWinner as getWinnerInternal } from './helpers/players.helpers';
+import { Game, Player } from './game.interfaces';
+
+export * from './game.interfaces';
+export { initializeGame } from './game';
+export { createCards, createCard } from './helpers/cards.helpers';
+
+export function getWinner(state: Game): string|null {
+    const winner: Player = getWinnerInternal(state.players);
+    return winner ? winner.id : null;
+}
